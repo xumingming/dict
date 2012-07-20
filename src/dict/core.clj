966724750cee7ef49ac03eb57b89fefa3c424410
James Xu <xumingming64398966@gmail.com>
2012-07-20 16:14:06 +0800

    basic implementation

--- src/dict/core.clj
+++ src/dict/core.clj
@@ -1,1 +1,22 @@
-(ns dict.core)
+(ns dict.core
+  (:require [clojure.xml :as xml])
+  (:gen-class))
+
+(defn fetch [word]
+  (let [url (str "http://dict.youdao.com/fsearch?q=" word)
+        body (xml/parse url)
+        content (-> body :content)
+        custom-translation (first (filter #(= :custom-translation (:tag %)) content))
+        custom-translation (:content custom-translation)
+        translations (filter #(= :translation (:tag %)) custom-translation)
+        translations (map #(-> % :content first :content first) translations)]
+    {:translations translations}))
+
+(defn look [word]
+  (let [translations (fetch word)
+        translations (:translations translations)]
+    (doseq [translation translations]
+      (println translation))))
+
+(defn -main [word]
+  (look word))
