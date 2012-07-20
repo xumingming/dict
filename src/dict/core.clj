b8779c4e6613060c98ae38019e992b85efd763e9
James Xu <xumingming64398966@gmail.com>
2012-07-20 23:11:57 +0800

    添加注音和网络解释

--- src/dict/core.clj
+++ src/dict/core.clj
@@ -4,19 +4,47 @@
 
 (defn fetch [word]
   (let [url (str "http://dict.youdao.com/fsearch?q=" word)
-        body (xml/parse url)
-        content (-> body :content)
-        custom-translation (first (filter #(= :custom-translation (:tag %)) content))
+        yodaodict (xml/parse url)
+        yodaodict (-> yodaodict :content)
+        custom-translation (first (filter #(= :custom-translation (:tag %)) yodaodict))
         custom-translation (:content custom-translation)
         translations (filter #(= :translation (:tag %)) custom-translation)
-        translations (map #(-> % :content first :content first) translations)]
-    {:translations translations}))
+        translations (map #(-> % :content first :content first) translations)
+        phonetic-symbol (filter #(= :phonetic-symbol (:tag %)) yodaodict)
+        phonetic-symbol (-> phonetic-symbol first :content first)
+        yodao-web-dict (first (filter #(= :yodao-web-dict (:tag %)) yodaodict))
+        web-translations (:content yodao-web-dict)
+        web-translations (map #(-> % :content) web-translations)
+        web-translations (map (fn [web-trans]
+                                (let [web-word (filter #(= :key (:tag %)) web-trans)
+                                      web-word (-> web-word first :content first)
+                                      transes (filter #(= :trans (:tag %)) web-trans)
+                                      transes (map #(-> % :content first :content first) transes)
+                                      ]
+                                  {:web-word web-word :transes transes}))
+                              web-translations)]
+    {:custom-translations translations
+     :web-translations web-translations
+     :phonetic-symbol phonetic-symbol}))
 
-(defn look [word]
-  (let [translations (fetch word)
-        translations (:translations translations)]
+(defn search [word]
+  (let [trans-result (fetch word)
+        translations (:custom-translations trans-result)
+        phonetic-symbol (:phonetic-symbol trans-result)
+        web-translations (:web-translations trans-result)]
+    (println "----- 发音 -----")
+    (println "\t" phonetic-symbol)
+    (println "----- 标准解释 -----")
     (doseq [translation translations]
-      (println translation))))
+      (println "\t" translation))
+    (println "----- 网络解释 -----")
+    (doseq [translation web-translations]
+      (let [web-word (:web-word translation)
+            transes (:transes translation)]
+        (print "\t" web-word " : ")
+        (doseq [trans transes]
+          (print trans ";")))
+      (println))))
 
 (defn -main [word]
-  (look word))
+  (search word))
