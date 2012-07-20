4ac1c6dedefeffd63a3aa133541a9ac59db10d91
James Xu <xumingming64398966@gmail.com>
2012-07-20 15:37:24 +0800

    init commit

---
+++ src/dict/core.clj
@@ -0,0 +1,1 @@
+(ns dict.core)
---
+++ test/dict/test/core.clj
@@ -0,0 +1,6 @@
+(ns dict.test.core
+  (:use [dict.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
